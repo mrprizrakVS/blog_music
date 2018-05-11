9d1074a464b8daf8207baa9c79224cad71e18c51
ryfys <mrprizrak.vs@gmail.com>
2018-05-12 01:38:48 +0300

    fix style

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -5,11 +5,17 @@
         @if(\Auth::check() && \Auth::user()->isAdmin == 1 )
             <a href="{{route('music.create')}}">
                 <button class="btn btn-primary">Create</button>
-            </a><br/>
+            </a>
+            <br/>
         @endif
         @foreach($musics as $music)
-           <a href="{{route('music.show', $music->id)}}">{{$music->name}} </a> (<small>{{$music->created_at->format('d-m-Y')}}</small>)<br/>
-
+            <div class="list-group">
+                <div class="list-group-item">
+                    <a href="{{route('music.show', $music->id)}}">{{$music->name}} </a> (
+                    <small>{{$music->created_at->format('d-m-Y')}}</small>
+                    )
+                </div>
+            </div>
 
         @endforeach
     </div>
